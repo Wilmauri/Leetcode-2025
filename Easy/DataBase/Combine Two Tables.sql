529dc9d54c2d0c5b807aa12b990409a5abda656d
Wilmauri Diaz <wilmauridiazcamacho@gmail.com>
2025-01-28 21:00:33 -0400

    New Solution Combine Two Tables.sql

---
+++ Easy/DataBase/Combine Two Tables.sql
@@ -0,0 +1,12 @@
+--https://leetcode.com/problems/combine-two-tables/description/
+SELECT
+    p.firstName
+    ,p.lastName
+    ,a.city
+    ,a.state
+FROM
+    Person AS p
+LEFT JOIN
+    Address AS a
+ON
+    p.personId = a.personId;
