220674afa660787923ead7f3907d4ad3eeed8e27
Wilmauri <wilmauridiazcamacho@gmail.com>
2025-02-02 20:25:12 -0400

    new solution

---
+++ Easy/Number/Special Array I.ts
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/special-array-i/description/
+function isArraySpecial(nums: number[]): boolean {
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] % 2 === 1 && nums[i + 1] % 2 === 1
+            || nums[i] % 2 === 0 && nums[i + 1] % 2 === 0) {
+            return false
+        }
+
+    }
+    return true
+};
+console.log(isArraySpecial([1]));
+console.log(isArraySpecial([2,1,4]));
+console.log(isArraySpecial([4,3,1,6]));
